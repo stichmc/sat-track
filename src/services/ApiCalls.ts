f20ae6707a14e6f33fb0fdd06abc78c7b5a2a179
tylerhand1 <tyha7462@colorado.edu>
2024-03-02 15:42:26 -0700

    Converted all Number type variables to number type

--- src/services/ApiCalls.ts
+++ src/services/ApiCalls.ts
@@ -3,19 +3,19 @@ const API_URL = `https://corsproxy.io/?https://api.n2yo.com/rest/v1/satellite/`;
 const API_KEY = import.meta.env.VITE_API_KEY
 
 interface InfoObject {
-    satid: Number;
+    satid: number;
     satname: String;
-    transactionscount: Number;
+    transactionscount: number;
 }
 
 interface PositionObject {
-    satlatitude: Number;
-    satlongitude: Number;
-    azimuth: Number;
-    elevation: Number;
-    ra: Number;
-    dec: Number;
-    timestamp: Number;
+    satlatitude: number;
+    satlongitude: number;
+    azimuth: number;
+    elevation: number;
+    ra: number;
+    dec: number;
+    timestamp: number;
 }
 
 export interface APIResponse {
@@ -23,7 +23,7 @@ export interface APIResponse {
     positions: Array<PositionObject>;
 }
 
-const getSatellitePosition = async (id: Number, count: Number) => {
+const getSatellitePosition = async (id: number, count: number) => {
     const latitude = 40.0074
     const longitude = -105.26633
     const elevation = 1655
