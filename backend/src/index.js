11332de906f6f549bea7ba3075843dc7d9ed9ecc
tylerhand1 <tyha7462@colorado.edu>
2024-03-03 00:05:44 -0700

    Added skibidirizz

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,13 +9,14 @@ import axios from "axios";
 // Declarations
 const app = express();
 const port = process.env.PORT;
-const API_URL = `https://corsproxy.io/?https://api.n2yo.com/rest/v1/satellite/`;
+const API_URL = `https://api.n2yo.com/rest/v1/satellite/`;
 const API_KEY = process.env.API_KEY;
 
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST"],
+    credentials: true,
   })
 );
 
@@ -38,9 +39,9 @@ try {
 app.use(bodyParser.json());
 
 const insertSatData = async () => {
-  const response = await getSatellitePosition(25544, 1);
+  const skibidiRizz = await getSatellitePosition(25544, 1);
 
-  // console.log(response);
+  console.log(skibidiRizz);
 };
 
 //@PARAMs id = satellite ID (from API),  
@@ -49,30 +50,39 @@ const getSatellitePosition = async (id, count) => {
   const longitude = -105.26633;
   const elevation = 1655;
 
-  const url = `${API_URL}positions/${id}/${latitude}/${longitude}/${elevation}/${count}/?apiKey=${API_KEY}`;
+  
+  try {
+    const url = `${API_URL}positions/${id}/${latitude}/${longitude}/${elevation}/${count}/?apiKey=${API_KEY}`;
 
-  console.log(url);
+    console.log(url);
 
-  const response = await axios.get(url);
+    const response = await axios.get(url);
 
-  console.log(response.status);
-  // console.log(response.data);
-
-  if (response.status === 200) {
-    const name = response.data.info.satname;
-    const id = response.data.info.satid;
-    const positions = response.data.positions;
-    const height = 6371 + 400; // Earth's radius + estimate
-    const responseData = { name, id, positions, height };
-
-    return responseData;
+    if (response.status === 200) {
+      const name = response.data.info.satname;
+      const id = response.data.info.satid;
+      const positions = response.data.positions;
+      const height = 6371 + 400; // Earth's radius + estimate
+      const responseData = { name, id, positions, height };
+  
+      return responseData;
+    } else {
+      const responseData = { name: "", id: -1, positions: [], height: -1 };
+      return responseData;
+    }
+  } catch (error) {
+    console.log(error)
+    if (!error.response) {
+      const responseData = { name: "", id: -1, positions: [], height: -1 }
+      return responseData;
   } else {
-    const responseData = { name: "", id: -1, positions: [], height: -1 };
-    return responseData;
+      const responseData = { name: "", id: -1, positions: [], height: -1 }
+      return responseData;
+  }
   }
 };
 
-// insertSatData();
+insertSatData();
 
 // Broadcasting
 if (port) {
