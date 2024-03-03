9fb48a46f2819f642ab421cea84447c3750eab84
CobhanK <cobhanjames@outlook.com>
2024-03-03 10:22:18 -0700

    Post method to search for any satellite

--- backend/postgres/create.sql
+++ backend/postgres/create.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS satellite;
 CREATE TABLE satellite (
     satID INT NOT NULL PRIMARY KEY,
     satname Varchar(60) NOT NULL,
-    launchDate DATE,
-    longtitude DECIMAL(10,7),
+    launchDate DATE DEFAULT CURRENT_DATE,
+    longitude DECIMAL(10,7),
     latitude DECIMAL(10,7),
     altitude FLOAT,
     time INT
